6574c34943325c12a27fb11fd40790cd3b346365
vlasovt <tarasvlasov@gmail.com>
2017-09-05 21:02:07 -0400

    added app setup for Wonderful Quote project

---
+++ 10. Wondorful Quotes Project/WonderfulQuotes/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                quotes: [
+                    "What a wonderful Quote..."
+                ],
+                maxQuotes: 10
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
