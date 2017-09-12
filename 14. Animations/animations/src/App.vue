994ad0cb54de663f23c4b51d20fc35a9979ac923
vlasovt <tarasvlasov@gmail.com>
2017-09-11 21:31:00 -0400

    added animation code (part1)

---
+++ 14. Animations/animations/src/App.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <select v-model="animationType" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br><br>
+                <button class="btn btn-primary" @click="show = !show">{{ buttonLabel }}</button>
+                <br><br>
+                <!-- you can only animate one element at a time inside the animation tag-->
+                <!-- css classes: *-enter, *-enter-active, *-leave, *-leave-active (defaul name: v-active)-->
+                <transition :name="animationType">
+                     <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition name="slide" type="animation" appear>
+                     <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition
+                 appear
+                 enter-active-class="animated bounce"
+                 leave-active-class="animated shake">
+                     <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: true,
+                animationType: 'fade'
+            }
+        },
+        computed:{
+            buttonLabel(){
+                return this.show? "Hide Info Alert": "Show Info Alert";
+            }
+        }
+    }
+</script>
+
+<style>
+    .fade-enter{
+        opacity: 0;
+    }
+
+    .fade-enter-active{
+        transition: opacity 1s;
+    }
+
+    .fade-leave{
+        /*opacity: 1; no need to set because the opacity goes to 1 by default*/
+    }
+
+    .fade-leave-active{
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter{
+        opacity: 0;
+        /*transform: translateY(20px);*/
+    }
+
+    .slide-enter-active{
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity 3s;
+    }
+
+    .slide-leave{
+        
+    }
+
+    .slide-leave-active{
+         animation: slide-out 1s ease-out forwards;
+          transition: opacity 1s;
+          opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from{
+            transform: translateY(20px);
+        }
+        to{
+             transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from{
+            transform: translateY(0);
+        }
+        to{
+             transform: translateY(20px);
+        }
+    }
+</style>
